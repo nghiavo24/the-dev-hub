5b36e1e75573a87fb93306e8e3cf9b2abbb2529a
mobrewer <morgiearan@gmail.com>
2022-12-23 12:56:31 -0700

    Added note and application to views.py

--- the_dev_hub_be/the_dev_hub/views.py
+++ the_dev_hub_be/the_dev_hub/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import PostingSerializer
-from .models import Posting
+from .serializers import PostingSerializer, ApplicationSerializer, NoteSerializer
+from .models import Posting, Application, Note
 # Create your views here.
 
 class PostingView(viewsets.ModelViewSet):
     serializer_class = PostingSerializer
     queryset = Posting.objects.all()
+
+class ApplicationView(viewsets.ModelViewSet):
+    serializer_class = ApplicationSerializer
+    queryset = Application.objects.all()
+
+class NoteView(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
