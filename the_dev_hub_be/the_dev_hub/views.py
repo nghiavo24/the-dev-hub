1bfc2952a9bd9a536d3a69d286a3c3e90b32012e
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-23 12:13:46 -0500

    add in the views page, a class to connect serializer and models for Posting

--- the_dev_hub_be/the_dev_hub/views.py
+++ the_dev_hub_be/the_dev_hub/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import PostingSerializer
+from .models import Posting
 # Create your views here.
+
+class PostingView(viewsets.ModelViewSet):
+    serializer_class = PostingSerializer
+    queryset = Posting.objects.all()
