96bad49fdc0939ef265b3ab161bed0ca342147af
mobrewer <morgiearan@gmail.com>
2022-12-23 12:46:52 -0700

    Made changes to admin.py, deleted code that was not needed

--- the_dev_hub_be/the_dev_hub/admin.py
+++ the_dev_hub_be/the_dev_hub/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from .models import Posting, Application, Note
-class JobAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'posted', 'url', 'note')
-
-class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'applied', 'hiring_manager', 'compensation', 'work_site', 'location')
 
 # Register your models here.
 
-admin.site.register(Posting, JobAdmin)
-admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Posting)
+admin.site.register(Application)
+admin.site.register(Note)
