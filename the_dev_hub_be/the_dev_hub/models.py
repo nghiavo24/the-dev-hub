5c932f79cc9902754b3af9049065d2c96822104b
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-23 10:10:00 -0600

    Add model for Posting

--- the_dev_hub_be/the_dev_hub/models.py
+++ the_dev_hub_be/the_dev_hub/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Posting(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=50)
+    posted = models.DateField()
+    url = models.TextField()
+    note = models.TextField(max_length=250, required = False)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
