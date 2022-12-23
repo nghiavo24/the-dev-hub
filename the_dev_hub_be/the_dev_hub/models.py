dd96ac84f6d80542dbed946f084fd18bd9343aff
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-23 10:17:12 -0600

    migrate completed

--- the_dev_hub_be/the_dev_hub/models.py
+++ the_dev_hub_be/the_dev_hub/models.py
@@ -5,7 +5,7 @@ class Posting(models.Model):
     company = models.CharField(max_length=50)
     posted = models.DateField()
     url = models.TextField()
-    note = models.TextField(max_length=250, required = False)
+    note = models.TextField(max_length=250, blank = True, null = True)
 
     def __str__(self):
         return self.title
