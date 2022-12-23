27ca2f4b6e7a81ba65633904257dafd41e2a06ae
mobrewer <morgiearan@gmail.com>
2022-12-23 12:42:31 -0700

    Added admin application, updated thedevhub admin to jobadmin

--- the_dev_hub_be/the_dev_hub/admin.py
+++ the_dev_hub_be/the_dev_hub/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Posting
-
-class TheDevHubAdmin(admin.ModelAdmin):
+from .models import Posting, Application, Note
+class JobAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'posted', 'url', 'note')
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'applied', 'hiring_manager', 'compensation', 'work_site', 'location')
 
 # Register your models here.
 
-admin.site.register(Posting, TheDevHubAdmin)
+admin.site.register(Posting, JobAdmin)
+admin.site.register(Application, ApplicationAdmin)
