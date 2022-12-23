17c17305fa482fd4ea3820eb62a4602d9a55727f
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-23 11:57:11 -0500

    add the dependncies to the settings.py/ with cores and restframework

--- the_dev_hub_be/the_dev_hub_be/settings.py
+++ the_dev_hub_be/the_dev_hub_be/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'the_dev_hub',
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'the_dev_hub_be.urls'
