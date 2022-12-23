d735c71d57b9aca0be54579db6a6d35e9b84c81b
mobrewer <morgiearan@gmail.com>
2022-12-23 12:53:47 -0700

    Added serializers for notes and application

--- the_dev_hub_be/the_dev_hub/serializers.py
+++ the_dev_hub_be/the_dev_hub/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers 
-from .models import Posting
+from .models import Posting, Application, Note
 
 class PostingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posting
         fields = ('id','title', 'company', 'posted', 'url', 'note')
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ('id', 'title', 'company', 'applied', 'hiring_manager', 'compensation', 'work_site', 'location')
+
+class NoteSerializer(serializers.ModelSerializer):
+    model = Note
+    fields = ('id', 'content')
