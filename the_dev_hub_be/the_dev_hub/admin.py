93523d32be1f7b7f1a5e4513b68088d6b4070ccd
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-23 10:23:07 -0600

    superuser created

--- the_dev_hub_be/the_dev_hub/admin.py
+++ the_dev_hub_be/the_dev_hub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Posting
+
+class TheDevHubAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'posted', 'url', 'note')
 
 # Register your models here.
+
+admin.site.register(Posting, TheDevHubAdmin)
