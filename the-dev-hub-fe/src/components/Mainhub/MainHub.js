4a9053863f47698c7bd0fec52bbe05339d9a39fb
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-23 18:03:38 -0600

    PUT request is not working, need to work on BE

--- the-dev-hub-fe/src/components/Mainhub/MainHub.js
+++ the-dev-hub-fe/src/components/Mainhub/MainHub.js
@@ -3,31 +3,66 @@ import axios from 'axios'
 
 const MainHub = () => {
     const [allPostings, setAllPostings] = useState()
+    const[updatePost, setUpdatePost]= useState({
+        title: "",
+        company: "",
+        posted: Date,
+        url: ""
+    })
 
-    const getAllPostings = (e) =>{
+    const getAllPostings = () =>{
         axios
         .get('http://localhost:8000/api/postings/')
         .then((res) => {setAllPostings(res.data)})
-        .catch()
+        .catch((err) => console.log(err))
     }
-console.log(allPostings)
 
-    
+    const updatePostingCall = async (e) =>{
+        e.preventDefault()
+        try{
+        await axios.put('http://localhost:8000/api/postings/2/', updatePost)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    const handleUpdateInput = (e) => {
+        e.preventDefault()
+        const postingUpdateInput = {...updatePost};
+        postingUpdateInput[e.target.name] = e.target.value;
+        setUpdatePost(postingUpdateInput)
+        console.log(updatePost)
+    }
 useEffect(() => {
     getAllPostings()
     
 }, []);
 
+console.log(allPostings)
+
 if(allPostings === undefined) return;
 
     let listOfPosting = allPostings.map((post, key) => {
         return(
-        <li>{post.title}</li>
+            <div>
+                <li>{post.title}</li>
+                <li>{post.posted}</li>
+            </div>
     )
 })
 
   return (
-    <div>{listOfPosting}</div>
+    <div>
+        <form onSubmit={updatePostingCall}>
+            <input placeholder='title' name='title' value={updatePost.title} onChange={handleUpdateInput}/>
+            <input placeholder='company' name='company' value={updatePost.company} onChange={handleUpdateInput}/>
+            <input placeholder='posted' name='posted' value={updatePost.posted} onChange={handleUpdateInput}/>
+            <input placeholder='url' name='url' value={updatePost.url} onChange={handleUpdateInput}/>
+            <button>Submit</button>
+        </form>
+        
+        {listOfPosting}
+    </div>
   )
 }
 
